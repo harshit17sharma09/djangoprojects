39a34e0a9f988d571492ffa911900570835dd71a
harshit17sharma09 <harshit17sharma09@gmail.com>
2018-06-03 19:08:20 +0530

    working on Groups,group base and detail template

--- socialweb/groups/views.py
+++ socialweb/groups/views.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
+
+from django.core.urlresolvers import reverse
+
+from django.views import generic
+from groups.Models import Group,GroupMember
+
+class CreateGroup(LoginRequiredMixin,generic.Createview):
+    fields = ('name','description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+     model =Group
+      
+
+class ListGroups(generic.ListView):
+    model = Group
